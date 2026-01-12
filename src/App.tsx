1b683390b83d3cabfe8e942baa80b9a783ca867f
Ricardo Nexus <ricardo@nexus.com>
2026-01-12 22:40:16 +0100

    implementación de la nueva navegación y el TradeModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Layout from './components/layout/Layout';
 import NewsIntelligencePanel from './components/dashboard/NewsIntelligencePanel';
 import PaperTradingPanel from './components/dashboard/PaperTradingPanel';
